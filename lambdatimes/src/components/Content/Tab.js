6543921165acbec425cfca176fe8cadcc8532c47
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-07 10:29:41 +0100

    Completed Tabs.js and Tab.js to display tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import pt from 'prop-types';
 
-const Tab = props => {
+const Tab = ({ tab, selectedTab }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={tab === selectedTab ? 'tab active-tab' : 'tab'}
+      onClick={(tab) => {selectedTab(tab)}}
     >
-      {props.tab.toUpperCase()}
+      {tab.toUpperCase()}
     </div>
   );
 };
@@ -20,3 +18,8 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+
+Tab.propTypes = {
+  tab: pt.string.isRequired,
+  selectedTab: pt.string.isRequired,
+}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import pt from 'prop-types';
+import uuidv1 from 'uuid/v1';
 import Tab from './Tab';
-const Tabs = props => {
+const Tabs = ({ tabs, selectedTab, selectedTabHandler }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+        tabs.map(tab => 
+          <Tab 
+            key={uuidv1()} 
+            selectedTabHandler={selectedTabHandler}
+            selectedTab={selectedTab}
+            tab={tab}
+          />)
+            }
       </div>
     </div>
   );
@@ -14,3 +24,9 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+  tabs: pt.array.isRequired,
+  selectedTab: pt.string.isRequired,
+  selectTabHandler: pt.func.isRequired
+}
